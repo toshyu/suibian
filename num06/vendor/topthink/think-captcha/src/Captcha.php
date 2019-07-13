6a081a43594edb051f28819074415de4f327f1ed
w28475125 <w28475125@163.com>
2019-07-13 13:46:49 +0800

    ·

--- num06/vendor/topthink/think-captcha/src/Captcha.php
+++ num06/vendor/topthink/think-captcha/src/Captcha.php
@@ -39,7 +39,7 @@ class Captcha
         // 验证码图片高度
         'imageW'   => 0,
         // 验证码图片宽度
-        'length'   => 6,
+        'length'   => 4,
         // 验证码位数
         'fontttf'  => '',
         // 验证码字体，不设置随机获取
