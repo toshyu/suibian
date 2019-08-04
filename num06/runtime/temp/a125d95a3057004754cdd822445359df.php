dc07a76b71a98b1ad90c2d46c2920334a8438bbb
toshyu <1181570632@qq.com>
2019-08-04 09:57:45 +0800

    提交

--- num06/runtime/temp/a125d95a3057004754cdd822445359df.php
+++ num06/runtime/temp/a125d95a3057004754cdd822445359df.php
@@ -1,4 +1,4 @@
-<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:73:"D:\phpstudy\PHPTutorial\WWW\num06/application/index\view\project\sel.html";i:1564881511;}*/ ?>
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:73:"D:\phpstudy\PHPTutorial\WWW\num06/application/index\view\project\sel.html";i:1564882151;}*/ ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -22,19 +22,18 @@
     <div class="layui-form">
         <div class="layui-row layui-form-item">
             <div class="  layui-col-md6">
-                <label class="layui-form-label">合同编号</label>
+                <label class="layui-form-label">合同编号：</label>
                 <div class="layui-form-mid layui-word-aux"><?php echo $vvo['order']; ?></div>
             </div>
-            <!--  -->
             <div class=" layui-col-md6">
-                <label class="layui-form-label">项目名称</label>
+                <label class="layui-form-label">项目名称：</label>
                 <div class="layui-form-mid layui-word-aux"><?php echo $vvo['project_name']; ?></div>
             </div>
         </div>
         <!--  -->
         <div class="layui-row layui-form-item ">
             <div class=" layui-col-md6">
-                <label class="layui-form-label">来源</label>
+                <label class="layui-form-label">来源：</label>
                 <div class="layui-form-mid layui-word-aux">
                     <?php if($vvo['source'] == 1): ?>合同
                     <?php elseif($vvo['source'] == 2): ?>备案
@@ -42,35 +41,33 @@
                     <?php else: ?>其他<?php endif; ?>
                 </div>
             </div>
-            <!--  -->
             <div class=" layui-col-md6">
-                <label class="layui-form-label">性质</label>
+                <label class="layui-form-label">性质：</label>
                 <div class="layui-form-mid layui-word-aux"><?php if($vvo['nature'] == 1): ?>市场招标
                     <?php elseif($vvo['nature'] == 2): ?>财政招标
                     <?php elseif($vvo['nature'] == 3): ?>合作挂靠
                     <?php else: ?>其他<?php endif; ?></div>
             </div>
+        </div>
+        <div class="layui-row layui-form-item ">
             <div class=" layui-col-md6">
-                <label class="layui-form-label">类型</label>
+                <label class="layui-form-label">类型：</label>
                 <div class="layui-input-block">
                     <div class="layui-form-mid layui-word-aux"><?php echo $vvo['type_title']; ?></div>
                 </div>
             </div>
-        </div>
-        <p></p>
-        <!--  -->
-        <div class="layui-row">
-            <div class="layui-form-item layui-col-md6">
+            <!--  -->
+            <div class=" layui-col-md6">
                 <label class="layui-form-label">委托单位：</label>
                 <div class="layui-input-block">
                     <div class="layui-form-mid layui-word-aux"><?php echo $vvo['unit_name']; ?></div>
                 </div>
             </div>
-            <div class=" layui-col-md6">
-                <label class="layui-form-label">委托单位行业</label>
-                <div class="layui-input-block">
-                    <div class="layui-form-mid layui-word-aux"><?php echo $vvo['unit_title']; ?></div>
-                </div>
+        </div>
+        <div class="layui-row layui-form-item">
+            <label class="layui-form-label">委托单位行业：</label>
+            <div class="layui-input-block">
+                <div class="layui-form-mid layui-word-aux"><?php echo $vvo['unit_title']; ?></div>
             </div>
         </div>
         <!--  -->
@@ -91,7 +88,7 @@
         <!--  -->
         <div class="layui-row layui-form-item ">
             <div class=" layui-col-md6">
-                <label class="layui-form-label">签订部门</label>
+                <label class="layui-form-label">签订部门：</label>
                 <div class="layui-input-block">
                     <div class="layui-form-mid layui-word-aux"><?php echo $vvo['sign_title']; ?></div>
                 </div>
